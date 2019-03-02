c59425669b95fd7fe17b396bd8e9ed1a65f825b2
Brian Alonso <brian.alonso.a@gmail.com>
2019-03-02 19:52:58 -0300

    Deploy to heroku

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+  res.status(200).send('Hello Continous Integration!!!');
 });
 module.exports = app;
